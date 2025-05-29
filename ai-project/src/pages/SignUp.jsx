4b88919eb1a89d7cac1551eeb478cbd6dfa34627
Roshni <roshnibarodiya1@gmail.com>
2025-05-30 01:37:45 +0530

    Update SignUp

---
+++ ai-project/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import bg from '../assets/bg.jpg'
+
+function SignUp ()  {
+  return (
+    <div className='w-full h-[100vh] ' 
+    style={{ backgroundImage: `url(${bg})` }}>
+        
+    </div>
+  )
+}
+
+export default SignUp
